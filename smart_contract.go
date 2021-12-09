2353f13491f08ec9ca041406dc92ef6d0a14b05e
Aung Maw <aungmawjj12@gmail.com>
2021-12-09 23:12:52 +0800

    modified

--- smart_contract.go
+++ smart_contract.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"encoding/base64"
-	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -14,11 +11,11 @@ import (
 func main() {
 	cc, err := contractapi.NewChaincode(&SmartContract{})
 	if err != nil {
-		log.Panicf("Error creating asset chaincode: %v", err)
+		log.Panicf("Error creating chaincode: %v", err)
 	}
 
 	if err := cc.Start(); err != nil {
-		log.Panicf("Error starting asset chaincode: %v", err)
+		log.Panicf("Error starting chaincode: %v", err)
 	}
 }
 
@@ -27,94 +24,171 @@ type SmartContract struct {
 }
 
 const (
-	KeyAssets = "assets"
+	KeyAssets        = "assets"
+	KeyAuctions      = "auctions"
+	KeyLastAuctionID = "lastAuction"
 )
 
 type Asset struct {
-	ID             []byte
-	Owner          []byte
-	PendingAuction *Auction
+	ID               string
+	Owner            string
+	PendingAuctionID int
 }
 
 type Auction struct {
-	ID       []byte
-	Platform string
+	ID              int
+	AssetID         string
+	Platforms       []string
+	CrossAuctionIDs []string
+	Status          string
+
+	HighestBid         int
+	HighestBidder      string
+	HighestBidPlatform string
 }
 
-type AuctionResult struct {
-	Auction
-	HighestBidder []byte
+type StartAuctionArgs struct {
+	AssetID   string
+	Platforms []string
 }
 
 type BindAuctionArgs struct {
-	AssetID []byte
-	Auction Auction
+	AuctionID       int
+	CrossAuctionIDs []string
 }
 
 type EndAuctionArgs struct {
-	AssetID       []byte
-	AuctionResult AuctionResult
+	AuctionID      int
+	HighestBids    []int
+	HighestBidders []string
 }
 
-func (cc *SmartContract) AddAsset(ctx contractapi.TransactionContextInterface, arg string) error {
-	var asset Asset
-	err := json.Unmarshal([]byte(arg), &asset)
-	if err != nil {
-		return err
+func (cc *SmartContract) AddAsset(
+	ctx contractapi.TransactionContextInterface, id, owner string,
+) error {
+	asset := Asset{
+		ID:    id,
+		Owner: owner,
 	}
 	return cc.setAsset(ctx, &asset)
 }
 
-func (cc *SmartContract) BindAuction(ctx contractapi.TransactionContextInterface, arg string) error {
-	var args BindAuctionArgs
-	err := json.Unmarshal([]byte(arg), &args)
+func (cc *SmartContract) StartAuction(
+	ctx contractapi.TransactionContextInterface, argJSON string,
+) error {
+	var args StartAuctionArgs
+	err := json.Unmarshal([]byte(argJSON), &args)
 	if err != nil {
 		return err
 	}
-	asset, err := cc.getAsset(ctx, args.AssetID)
+
+	asset, err := cc.GetAsset(ctx, args.AssetID)
 	if err != nil {
 		return err
 	}
-	asset.PendingAuction = &args.Auction
+	if asset.PendingAuctionID > 0 {
+		return fmt.Errorf("pending auction on asset")
+	}
+
+	lastID, err := cc.GetLastAuctionID(ctx)
+	if err != nil {
+		return err
+	}
+	auction := Auction{
+		ID:              lastID + 1,
+		AssetID:         args.AssetID,
+		Platforms:       args.Platforms,
+		CrossAuctionIDs: make([]string, len(args.Platforms)),
+		Status:          "Started",
+	}
+	err = cc.setAuction(ctx, &auction)
+	if err != nil {
+		return err
+	}
+	err = cc.setLastAuctionID(ctx, auction.ID)
+	if err != nil {
+		return err
+	}
+
+	asset.PendingAuctionID = auction.ID
 	return cc.setAsset(ctx, asset)
 }
 
-func (cc *SmartContract) EndAuction(ctx contractapi.TransactionContextInterface, arg string) error {
+func (cc *SmartContract) BindAuction(
+	ctx contractapi.TransactionContextInterface, argJSON string,
+) error {
+	var args BindAuctionArgs
+	err := json.Unmarshal([]byte(argJSON), &args)
+	if err != nil {
+		return err
+	}
+	auction, err := cc.GetAuction(ctx, args.AuctionID)
+	if err != nil {
+		return err
+	}
+	auction.Status = "Bind"
+	auction.CrossAuctionIDs = args.CrossAuctionIDs
+	return cc.setAuction(ctx, auction)
+}
+
+func (cc *SmartContract) SetAuctionEnding(
+	ctx contractapi.TransactionContextInterface, assetID string,
+) error {
+	asset, err := cc.GetAsset(ctx, assetID)
+	if err != nil {
+		return err
+	}
+	auction, err := cc.GetAuction(ctx, asset.PendingAuctionID)
+	if err != nil {
+		return err
+	}
+	auction.Status = "Ending"
+	return cc.setAuction(ctx, auction)
+}
+func (cc *SmartContract) EndAuction(
+	ctx contractapi.TransactionContextInterface, argJSON string,
+) error {
+
 	var args EndAuctionArgs
-	err := json.Unmarshal([]byte(arg), &args)
+	err := json.Unmarshal([]byte(argJSON), &args)
 	if err != nil {
 		return err
 	}
-	asset, err := cc.getAsset(ctx, args.AssetID)
+	auction, err := cc.GetAuction(ctx, args.AuctionID)
 	if err != nil {
 		return err
-	}
-	if asset.PendingAuction == nil {
-		return fmt.Errorf("no pending auction")
-	}
-	if !bytes.Equal(asset.PendingAuction.ID, args.AuctionResult.ID) {
-		return fmt.Errorf("invalid auction result")
 	}
 
-	// transfer asset to winner
-	asset.Owner = args.AuctionResult.HighestBidder
-	asset.PendingAuction = nil
-	return cc.setAsset(ctx, asset)
+	for idx, bid := range args.HighestBids {
+		if bid > auction.HighestBid {
+			auction.HighestBid = bid
+			auction.HighestBidPlatform = auction.Platforms[idx]
+			auction.HighestBidder = args.HighestBidders[idx]
+		}
+	}
+
+	auction.Status = "Ended"
+	err = cc.setAuction(ctx, auction)
+	if err != nil {
+		return err
+	}
+
+	asset, err := cc.GetAsset(ctx, auction.AssetID)
+	if err != nil {
+		return err
+	}
+
+	asset.Owner = auction.HighestBidder
+	asset.PendingAuctionID = 0
+	err = cc.setAsset(ctx, asset)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (cc *SmartContract) GetAsset(
-	ctx contractapi.TransactionContextInterface, arg string,
-) (string, error) {
-	assetID, err := base64.StdEncoding.DecodeString(arg)
-	if err != nil {
-		return "", err
-	}
-	b, err := ctx.GetStub().GetState(cc.makeAssetKey(assetID))
-	return string(b), err
-}
-
-func (cc *SmartContract) getAsset(
-	ctx contractapi.TransactionContextInterface, assetID []byte,
+	ctx contractapi.TransactionContextInterface, assetID string,
 ) (*Asset, error) {
 	var asset Asset
 	b, err := ctx.GetStub().GetState(cc.makeAssetKey(assetID))
@@ -128,17 +202,66 @@ func (cc *SmartContract) getAsset(
 	return &asset, err
 }
 
+func (cc *SmartContract) GetAuction(
+	ctx contractapi.TransactionContextInterface, auctionID int,
+) (*Auction, error) {
+	b, err := ctx.GetStub().GetState(cc.makeAuctionKey(auctionID))
+	if err != nil {
+		return nil, err
+	}
+	if b == nil {
+		return nil, fmt.Errorf("auction not found")
+	}
+	var auction Auction
+	err = json.Unmarshal(b, &auction)
+	return &auction, err
+}
+
+func (cc *SmartContract) GetLastAuctionID(
+	ctx contractapi.TransactionContextInterface,
+) (int, error) {
+	b, err := ctx.GetStub().GetState(KeyLastAuctionID)
+	if err != nil {
+		return 0, err
+	}
+	var count int
+	json.Unmarshal(b, &count)
+	return count, nil
+}
+
 func (cc *SmartContract) setAsset(
 	ctx contractapi.TransactionContextInterface, asset *Asset,
 ) error {
 	b, _ := json.Marshal(asset)
 	err := ctx.GetStub().PutState(cc.makeAssetKey(asset.ID), b)
 	if err != nil {
-		return fmt.Errorf("put state error %v", err)
+		return fmt.Errorf("set asset error: %v", err)
 	}
 	return nil
 }
 
-func (cc *SmartContract) makeAssetKey(assetID []byte) string {
-	return fmt.Sprintf("%s_%s", KeyAssets, hex.EncodeToString(assetID))
+func (cc *SmartContract) setAuction(
+	ctx contractapi.TransactionContextInterface, auction *Auction,
+) error {
+	b, _ := json.Marshal(auction)
+	err := ctx.GetStub().PutState(cc.makeAuctionKey(auction.ID), b)
+	if err != nil {
+		return fmt.Errorf("set auction error: %v", err)
+	}
+	return nil
+}
+
+func (cc *SmartContract) setLastAuctionID(
+	ctx contractapi.TransactionContextInterface, id int,
+) error {
+	b, _ := json.Marshal(id)
+	return ctx.GetStub().PutState(KeyLastAuctionID, b)
+}
+
+func (cc *SmartContract) makeAssetKey(assetID string) string {
+	return fmt.Sprintf("%s_%s", KeyAssets, assetID)
+}
+
+func (cc *SmartContract) makeAuctionKey(auctionID int) string {
+	return fmt.Sprintf("%s_%d", KeyAuctions, auctionID)
 }
